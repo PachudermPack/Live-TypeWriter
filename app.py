4a365f1b8f4c07087913c0e1d1fdb372eca16953
PachudermPack <164845089+PachudermPack@users.noreply.github.com>
2025-09-15 12:58:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/text')
-def get_text():
-    filename = "text-to-type.txt"
-    if not os.path.exists(filename):
-        return jsonify({"error": "File not found"}), 404
-
-    with open(filename, "r", encoding="utf-8") as f:
-        content = f.read()
-    return jsonify({"text": content})
-
 def run_server():
     app.run(port=5000)
 
 if __name__ == "__main__":
     threading.Timer(1.0, lambda: webbrowser.open("http://127.0.0.1:5000/")).start()
+
     run_server()
